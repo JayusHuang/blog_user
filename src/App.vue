99e75e5fa76320795e542be1bacf14585479dfe1
jayus <jayusHug@outlook.com>
2022-12-15 20:15:01 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
  <template>
-  <div id="#app">
-    <div><router-link to="/"> Home</router-link></div>
-    <div><router-link to="/HelloWorld"> HelloWorld</router-link></div>
-    <div><router-link to="/GameCard"> GameCard </router-link></div>
-    <router-link to="/BookCard">BookCard</router-link>
-    <router-view />
+  <div id="#app" class="common-layout">
+    <el-container>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>Workspace</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-sub-menu index="2-4">
+              <template #title>item four</template>
+              <el-menu-item index="2-4-1">item one</el-menu-item>
+              <el-menu-item index="2-4-2">item two</el-menu-item>
+              <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="3">Info</el-menu-item>
+          <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu></el-header
+      >
+      <el-main
+        >Main
+        <div>
+          <div><router-link to="/"> Home</router-link></div>
+          <div><router-link to="/HelloWorld"> HelloWorld</router-link></div>
+          <div><router-link to="/GameCard"> GameCard </router-link></div>
+          <router-link to="/BookCard">BookCard</router-link>
+          <router-view />
+        </div>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <style scoped>
 </style>
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
